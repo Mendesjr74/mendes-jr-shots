7ebd346ec7e77f1fc477f3f31ceeb357de1efc46
Mendes Jr <mendesjr.fortal@gmail.com>
2026-04-21 16:53:25 -0300

    corrigir URL do site (mendesjrshots, sem h\u00edfen)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://mendesjr-shots.netlify.app',
+  site: 'https://mendesjrshots.netlify.app',
 });
